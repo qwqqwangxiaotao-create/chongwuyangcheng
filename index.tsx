a1d9651217bfe9019f524918b2e65375245ad39e
qwqqwangxiaotao-create <qwqqwangxiaotao@gmail.com>
2025-11-21 17:17:35 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// 1. 必须引入 BrowserRouter，如果没有这一行会报错
+import { BrowserRouter } from 'react-router-dom'; 
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -10,6 +12,9 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    {/* 2. 在这里包裹 App，并加上 basename */}
+    <BrowserRouter basename="/chongwuyangcheng">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
