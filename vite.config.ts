9ed65237c0145b3ff11a5708165719b9972c311c
qwqqwangxiaotao-create <qwqqwangxiaotao@gmail.com>
2025-11-21 17:41:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // 核心修改：添加 base 路径，必须和你的仓库名一致，前后都要有斜杠
-      base: '/chongwuyangcheng/',
-
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
+      base: '/chongwuyangcheng/', // 这一行千万不能丢！
       plugins: [react()],
       define: {
-        // 注意：这里的 Key 会被打包到前端代码中，公开可见
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
